c1e8b729fdb28719597c42d8618da213daf8a9ee
Thomas Ferrant <ferrantthomas@hotmail.fr>
2019-05-24 12:44:20 +0900

    fixed create cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,5 @@
 class CocktailsController < ApplicationController
-  before_action :set_ingredient, only: [:show, :edit, :destroy]
+  before_action :set_cocktail, only: [:show, :edit, :destroy]
 
   def index
     @cocktails = Cocktail.all
@@ -40,7 +40,7 @@ class CocktailsController < ApplicationController
 
   private
 
-  def set_ingredient
+  def set_cocktail
     @cocktail = Cocktail.find(params[:id])
   end
 
